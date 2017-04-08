ec2a95d917b3790770c2fe26e4b4c8a641d7ac84
williamthompson <s3533301@student.rmit.edu.au>
2017-04-08 17:44:36 +1000

    Incorporates methods for CourseOffering.

--- src/model/CourseOffering.java
+++ src/model/CourseOffering.java
@@ -1,9 +1,44 @@
 package model;
 
-class CourseOffering extends Course {
+public class CourseOffering {
+	// Define a Course Offering
 
-    private Semester semester;
-    private int maxStudents;
-    private Lecturer lecturer;
+		// Declare and initialize the class variables
+		String name = "Software Engineering Fundamentals";
+		int semester = 2017;
+		int maxStudents = 30;
+		String lecturer = "Sam";
+		
+		// Declare and initialise the static variables
+		static int numberOfCourseOfferings = 0;
+		
+		// Construct a Course Offering
+		CourseOffering(){
+			numberOfCourseOfferings++;
+		}
+		
+		// Construct a Course Offering
+		CourseOffering(String name, int semester, int maxStudents, String lecturer){
+			numberOfCourseOfferings++;
+		}
 
+		// Set the namer of the Course Offering
+		void setName(String newName){
+			name = newName;
+		}
+		
+		// Set the semester of the Course Offering
+		void setSemester(int newSemester){
+			semester = newSemester;
+		}
+		
+		// Set the maximum number of students
+		void setMaxStudents(int maxNoStudents){
+			maxStudents = maxNoStudents;
+		}
+		
+		// Assign the Lecturer
+		void assignLecturerCourse(String assignedLecturer){
+			lecturer = assignedLecturer;
+		}
 }
