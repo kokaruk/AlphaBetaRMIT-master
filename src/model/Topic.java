37957fce2fe410cf5a594cf82793e55f8f24203b
rmit-s3533301-william-thompson <s3533301@student.rmit.edu.au>
2017-04-09 11:24:55 +1000

    A Topic class and TopicTest JUnit test.

--- src/model/Topic.java
+++ src/model/Topic.java
@@ -1,7 +1,42 @@
 package model;
 
+/**
+ * Created by Bill Thompson on 9/4/17.
+ * Topic Class
+ */
+
 class Topic {
 
-    String name;
-
+	// Define and initialize the class variables
+    private String name ;
+    
+	// Declare and initialise the static variables
+	public static int numberOfTopics = 0;
+	
+	// Construct a Topic with a particular name
+	public Topic(String topicName){
+		this.name = topicName;
+		numberOfTopics++;
+	}
+	
+	// Get the number of Topics
+	public int getNumberOfTopics(){
+		return numberOfTopics;
+	}
+	
+	// Construct a Topic with a blank name
+	public Topic(){
+		numberOfTopics++;
+	}
+	
+	// Set the name of the topic
+	public void setName(String newName){
+		this.name = newName;
+	}
+	
+	// Get the name of the Topic
+	public String getNameOfTopic(){
+		return name;
+	}
+    
 }
