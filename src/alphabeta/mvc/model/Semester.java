090d020766d35dcbcd6fa6a129ed70a8dbfc73df
rmit-s3586931-kristin-stenland <s3586931@student.rmit.edu.au>
2017-05-09 21:51:35 +1000

    K from IJ

--- src/alphabeta/mvc/model/Semester.java
+++ src/alphabeta/mvc/model/Semester.java
@@ -4,4 +4,28 @@ public class Semester {
     public int semesterNumber;
     public int year;
     public int week;
+
+    public int getSemesterNumber() {
+        return semesterNumber;
+    }
+
+    public void setSemesterNumber(int semesterNumber) {
+        this.semesterNumber = semesterNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
 }
