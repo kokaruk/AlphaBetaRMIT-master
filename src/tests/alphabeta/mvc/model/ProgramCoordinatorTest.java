700c855e941623b71dcc3c49b8f039befee8f8e8
rmit-s3668804-Dzmitry-Kakaruk <s3668804@studend.rmit.edu.au>
2017-05-11 00:09:33 +1000

    new PMTestClass

---
+++ src/tests/alphabeta/mvc/model/ProgramCoordinatorTest.java
@@ -0,0 +1,40 @@
+package alphabeta.mvc.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author dimz
+ * @since 11/5/17.
+ */
+public class ProgramCoordinatorTest {
+    @Test
+    public void addNewCourse() throws Exception {
+    }
+
+    @Test
+    public void grantWaivers() throws Exception {
+    }
+
+    @Test
+    public void increaseLoad() throws Exception {
+    }
+
+    @Test
+    public void viewAllResults() throws Exception {
+    }
+
+    @Test
+    public void getName() throws Exception {
+    }
+
+    @Test
+    public void getUsername() throws Exception {
+    }
+
+    @Test
+    public void viewCourseOfferings() throws Exception {
+    }
+
+}
