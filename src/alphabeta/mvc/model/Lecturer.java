7282e6e2ad2034cccf6bfe649b4ebb45602ef89c
rmit-s3586931-kristin-stenland <s3586931@student.rmit.edu.au>
2017-05-14 21:23:31 +1000

    K - 14 may. Lecturer not finished....

--- src/alphabeta/mvc/model/Lecturer.java
+++ src/alphabeta/mvc/model/Lecturer.java
@@ -14,8 +14,11 @@ public class Lecturer extends Staff {
     Set<Course> myCourse;
 
     public boolean upLoadResults(Student student, Result result) throws NoSuchElementException, ItemExistsException{
+        boolean uploaded;
         // TODO - implement alphabeta.mvc.model.Lecturer.upLoadResults
         //throw new UnsupportedOperationException();
+        //if student not enrolled in course throw new NoSuchElementException()
+        //if student already has a result saved for the course, throw ItemExistsException();
         return false;
     }
 
