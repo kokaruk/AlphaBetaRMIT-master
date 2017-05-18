14d34fa6ea163828cc7eaa965122882ed3c682f5
rmit-s3586931-kristin-stenland <s3586931@student.rmit.edu.au>
2017-05-18 22:45:53 +1000

    K - addLecturerController minor update

--- src/alphabeta/mvc/controller/addLecturerController.java
+++ src/alphabeta/mvc/controller/addLecturerController.java
@@ -3,7 +3,7 @@ package alphabeta.mvc.controller;
 import alphabeta.mvc.model.CourseDirectory;
 import alphabeta.mvc.model.CourseOffering;
 import alphabeta.mvc.model.Lecturer;
-import alphabeta.mvc.model.Semester;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,6 +17,7 @@ import java.util.Set;
 
 /**
  * Created by Kristin on 18/05/2017.
+ * Controller for the view that allows a Admin to select a new lecturer for a course offering.
  */
 public class addLecturerController {
 
@@ -24,8 +25,6 @@ public class addLecturerController {
 
     @FXML
     private ComboBox<String> courseName;
-    @FXML
-    private ComboBox<Semester> semesterNumber;
     @FXML
     private ComboBox<Lecturer> lecturer;
     @FXML
@@ -37,10 +36,13 @@ public class addLecturerController {
     public void initialize() {
         // create strings of all course names, semesters and lecturers from the course offerings directory
         // and display in the drop downs
-        courseDirectory.getCourseOfferingSet();
-
-
-
+        Set<CourseOffering> co = courseDirectory.getCourseOfferingSet();
+        List<String> courseStrings = new ArrayList<>();
+        for (CourseOffering c : co) {
+            courseStrings.add(c.getName() + ": Semester 0" + c.getMySemester().getSemesterNumber() + " " + c.getMySemester().getYear());
+        }
+        ObservableList<String> displayCourseNames = FXCollections.observableArrayList(courseStrings);
+        courseName.getItems().addAll(displayCourseNames);
     }
 
     public void saveButtonClick(ActionEvent event) {
@@ -51,7 +53,4 @@ public class addLecturerController {
 
     }
 
-    public void displayCourseOfferings(){
-        CourseDirectory.getInstance().getSemester();
-    }
 }
